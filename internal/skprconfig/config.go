a83f8d95878c38ea23d9b605241ec370e65e56ec
Nick Santamaria <nick.santamaria@previousnext.com.au>
2019-08-06 01:04:04 +1000

    Initial commit of skprconfig package.

---
+++ internal/skprconfig/config.go
@@ -0,0 +1,65 @@
+package skprconfig
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+const (
+	DefaultPath string = "/etc/skpr"
+	DefaultTrimSuffix string = "\n"
+)
+
+// Get returns the configured value of a given key, and the fallback value if no
+// key does not exist.
+//
+// This is a convenience if the default skpr config mount point is being used.
+func Get(key, fallback string) string {
+	c := NewConfig(DefaultPath, DefaultTrimSuffix)
+	return c.Get(key, fallback)
+}
+
+
+// Config holds parameters for config.
+type Config struct {
+	Path string
+	TrimSuffix string
+}
+
+// NewConfig returns a new Config struct with a given set of parameters.
+func NewConfig(path, trimSuffix string) *Config {
+	return &Config{
+		Path: path,
+		TrimSuffix: trimSuffix,
+	}
+}
+
+// Get returns the configured value of a given key, and the fallback value if no
+// key does not exist.
+func(c *Config) Get(key, fallback string) string {
+	value, err := c.GetWithError(key)
+	if err != nil {
+		return fallback
+	}
+
+	return value
+}
+
+// GetWithError returns the configured value of a given key.
+func(c *Config) GetWithError(key string) (string, error) {
+	file := fmt.Sprintf("%s/%s", c.Path, key)
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return "", err
+	}
+
+	contents, err := ioutil.ReadFile(file)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSuffix(string(contents), "\n"), nil
+}
+
