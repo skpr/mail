6d18bed80f9c1f4acbd05fb7270b9f87c47db911
Nick Santamaria <nick.santamaria@previousnext.com.au>
2019-08-06 10:33:28 +1000

    Read mail from stdin and send via ses.

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -26,8 +27,7 @@ func main() {
 	var params Params
 	err := envconfig.Process("skprmail", &params)
 	if err != nil {
-		fmt.Println("Could not process configuration")
-		os.Exit(1)
+		log.Fatal("Could not process configuration")
 	}
 
 	// Check for skpr config values for any parameters in Params which are empty.
@@ -35,38 +35,54 @@ func main() {
 	if params.AwsAccessKeyId == "" {
 		params.AwsAccessKeyId, err = c.GetWithError("smtp.username")
 		if err != nil {
-			fmt.Println("AWS credentials not configured")
-			os.Exit(1)
+			log.Fatal("AWS credentials not configured")
 		}
 	}
 	if params.AwsSecretAccessKey == "" {
 		params.AwsSecretAccessKey, err = c.GetWithError("smtp.password")
 		if err != nil {
-			fmt.Println("AWS credentials not configured")
-			os.Exit(1)
+			log.Fatal("AWS credentials not configured")
 		}
 	}
 	if params.AwsRegion == "" {
 		params.AwsRegion, err = c.GetWithError("smtp.region")
 		if err != nil {
-			fmt.Println("AWS region not configured")
-			os.Exit(1)
+			log.Fatal("AWS region not configured")
 		}
 	}
 	if params.FromAddress == "" {
 		params.FromAddress, err = c.GetWithError("smtp.region")
 		if err != nil {
-			fmt.Println("FROM address not configured. This may impact deliverability of the message.")
+			log.Println("FROM address not configured. This may impact deliverability of the message.")
 		}
 	}
 
 	sess, err := awsSession(params)
 	if err != nil {
-		fmt.Println("Could not initialise AWS session.")
-		os.Exit(1)
+		// @todo fallback to smtp forwarding (i.e. mailhog) if aws session not available.
+		log.Fatal("AWS region not configured")
 	}
 	client := ses.New(sess)
 
+	// Load input from Stdin and build up raw mail object.
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("failed to read message from stdin: %s", err)
+	}
+	mailinput := &ses.SendRawEmailInput{
+		RawMessage: &ses.RawMessage{Data: input},
+		Source:     &params.FromAddress,
+	}
+
+	// Send email.
+	output, err := client.SendRawEmail(mailinput)
+	if err != nil {
+		log.Fatalf("failed to send message: %s", err)
+	}
+
+	// Everything looks OK!
+	log.Printf("message id %s", *output.MessageId)
+	os.Exit(0)
 }
 
 // Helper function which creates AWS session.
