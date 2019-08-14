4164d05bcefbc95ebc0242fef69ac5cc36f7266b
Nick Schuch <nick@previousnext.com.au>
2019-08-14 15:16:48 +1000

    Set Reply-To with original From. Preserve Reply-To if already set. (#4)

--- internal/mailutils/mailutils_test.go
+++ internal/mailutils/mailutils_test.go
@@ -8,6 +8,117 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestEnforceFrom(t *testing.T) {
+	scenarios := []struct {
+		Message  *mail.Message
+		From     string
+		Expected *mail.Message
+	}{
+		// No From address was specified.
+		{
+			Message: &mail.Message{
+				Header: mail.Header{
+					HeaderTo: []string{
+						"to@example.com",
+					},
+				},
+				Body: strings.NewReader("This is a test"),
+			},
+			From: "override@example.com",
+			Expected: &mail.Message{
+				Header: mail.Header{
+					HeaderTo: []string{
+						"to@example.com",
+					},
+					HeaderFrom: []string{
+						"override@example.com",
+					},
+					HeaderSender: []string{
+						"override@example.com",
+					},
+					HeaderReplyTo: []string{
+						"override@example.com",
+					},
+				},
+				Body: strings.NewReader("This is a test"),
+			},
+		},
+		// No Reply-To header was set.
+		{
+			Message: &mail.Message{
+				Header: mail.Header{
+					HeaderTo: []string{
+						"to@example.com",
+					},
+					HeaderFrom: []string{
+						"from@example.com",
+					},
+				},
+				Body: strings.NewReader("This is a test"),
+			},
+			From: "override@example.com",
+			Expected: &mail.Message{
+				Header: mail.Header{
+					HeaderTo: []string{
+						"to@example.com",
+					},
+					HeaderFrom: []string{
+						"override@example.com",
+					},
+					HeaderSender: []string{
+						"override@example.com",
+					},
+					HeaderReplyTo: []string{
+						"from@example.com",
+					},
+				},
+				Body: strings.NewReader("This is a test"),
+			},
+		},
+		// Don't override existing Reply-To header.
+		{
+			Message: &mail.Message{
+				Header: mail.Header{
+					HeaderTo: []string{
+						"to@example.com",
+					},
+					HeaderFrom: []string{
+						"from@example.com",
+					},
+					HeaderReplyTo: []string{
+						"alreadyoverridden@example.com",
+					},
+				},
+				Body: strings.NewReader("This is a test"),
+			},
+			From: "override@example.com",
+			Expected: &mail.Message{
+				Header: mail.Header{
+					HeaderTo: []string{
+						"to@example.com",
+					},
+					HeaderFrom: []string{
+						"override@example.com",
+					},
+					HeaderSender: []string{
+						"override@example.com",
+					},
+					HeaderReplyTo: []string{
+						"alreadyoverridden@example.com",
+					},
+				},
+				Body: strings.NewReader("This is a test"),
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		err := EnforceFrom(scenario.Message, scenario.From)
+		assert.Nil(t, err)
+		assert.Equal(t, scenario.Expected.Header, scenario.Message.Header)
+	}
+}
+
 func TestMessageToBytes(t *testing.T) {
 	msg := &mail.Message{
 		Header: mail.Header{
