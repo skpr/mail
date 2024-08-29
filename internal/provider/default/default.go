1bce2f0bd62aa75bb8ba971bb82c0cc108493456
Karl Hepworth <karl@previousnext.com.au>
2024-08-29 10:59:39 +1000

    add skpr config support to the default provider
    Signed-off-by: Karl Hepworth <karl@previousnext.com.au>

--- internal/provider/default/default.go
+++ internal/provider/default/default.go
@@ -2,12 +2,14 @@ package local
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/mail"
 	"net/smtp"
 	"os"
 
+	skprconfig "github.com/skpr/go-config"
 	"github.com/skpr/mail/internal/mailutils"
 )
 
@@ -16,10 +18,20 @@ const (
 	EnvAddr = "SKPRMAIL_ADDR"
 	// EnvFrom used to configure the FROM address appled to mail.
 	EnvFrom = "SKPRMAIL_FROM"
+	// EnvPort used to configure the port for the destination address.
+	EnvPort = "SKPRMAIL_PORT"
 	// FallbackAddr where mail will be forwarded to.
-	FallbackAddr = "mail:1025"
+	FallbackAddr = "mail"
 	// FallbackFrom address which will be applied to email.
 	FallbackFrom = "skprmail"
+	// FallbackPort address which will be applied to email.
+	FallbackPort = "1025"
+	// ConfigAddr used to override and set the address where mail is sent using a Skpr config.
+	ConfigAddr = "smtp.hostname"
+	// ConfigFrom used to override and set the FROM address where mail is sent using a Skpr config.
+	ConfigFrom = "smtp.from.address"
+	// ConfigPort used to override and set the address port where mail is sent using a Skpr config.
+	ConfigPort = "smtp.port"
 )
 
 // Send the email to Mailhog.
@@ -32,6 +44,11 @@ func Send(ctx context.Context, to []string, msg *mail.Message) error {
 		os.Exit(1)
 	}()
 
+	config, err := skprconfig.Load()
+	if err != nil && !errors.Is(err, skprconfig.ErrNotFound) {
+		panic(err)
+	}
+
 	data, err := mailutils.MessageToBytes(msg)
 	if err != nil {
 		return err
@@ -43,15 +60,22 @@ func Send(ctx context.Context, to []string, msg *mail.Message) error {
 
 	addr := os.Getenv(EnvAddr)
 	if addr == "" {
-		addr = FallbackAddr
+		addr = config.GetWithFallback(ConfigAddr, FallbackAddr)
+	}
+
+	port := os.Getenv(EnvPort)
+	if port == "" {
+		port = config.GetWithFallback(ConfigPort, FallbackPort)
 	}
 
 	from := os.Getenv(EnvFrom)
 	if from == "" {
-		addr = FallbackFrom
+		from = config.GetWithFallback(ConfigFrom, FallbackFrom)
 	}
 
-	err = smtp.SendMail(addr, nil, from, to, data)
+	destination := fmt.Sprintf("%s:%s", addr, port)
+
+	err = smtp.SendMail(destination, nil, from, to, data)
 	if err != nil {
 		return fmt.Errorf("failed to send message via mailhog smtp %w", err)
 	}
